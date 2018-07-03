ea9ac8a426e11cd9b58ff54424ae38876a20a00c
David McCormick <dave.a.mccormick@gmail.com>
2018-07-03 22:01:30 +1000

    Better menu

--- pimpedmenu.jsx
+++ pimpedmenu.jsx
@@ -2,18 +2,24 @@ import React, {Component} from 'react';
 import { push as Menu } from 'react-burger-menu';
 
 export default class PimpedMenu extends Component {
-  showSettings (event) {
-    // event.preventDefault();
-  }
-
   render () {
     return (
         <div id="outer-container">
-            <Menu isOpen={ true } pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }>
-                <a id="home" className="menu-item" href="/">Home</a>
-                <a id="about" className="menu-item" href="/about">About</a>
-                <a id="contact" className="menu-item" href="/contact">Contact</a>
-                <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+            <Menu 
+                isOpen={ true } 
+                pageWrapId={ "page-wrap" } 
+                outerContainerId={ "outer-container" }
+                width={350}
+            >
+                <div className="crosshair section">
+                  <nav className="link-effect-14" id="link-effect-14">
+                      <a href="#link-effect-14"><span>Introduction</span></a>
+                      <a href="#link-effect-14"><span>Prototype Overview</span></a>
+                      <a href="#link-effect-14"><span>Add Face</span></a>
+                      <a href="#link-effect-14"><span>Other Stuff</span></a>
+                      <a href="#link-effect-14"><span>Blah Blah</span></a>
+                  </nav>
+                </div>
             </Menu>
             <main id="page-wrap">
                 <div>This is a test</div>
