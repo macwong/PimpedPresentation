899d300936e5fba0bed8cbf966262f7ba8fe23c3
David McCormick <dave.a.mccormick@gmail.com>
2018-07-05 22:31:46 +1000

    Go frameless

--- main.js
+++ main.js
@@ -7,7 +7,11 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1024, height: 768})
+  mainWindow = new BrowserWindow({
+    width: 1024, 
+    height: 768, 
+    frame: false
+  })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
