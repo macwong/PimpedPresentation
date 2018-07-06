03765ebcb38127b687f904855e77fd8c088b2420
David McCormick <dave.a.mccormick@gmail.com>
2018-07-06 10:11:35 +1000

    Move react components to folder

--- src/renderer.js
+++ src/renderer.js
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PimpedMenu from '../pimpedmenu';
+import PimpedMenu from '../src/components/pimpedmenu';
 
 ReactDOM.render(
     <PimpedMenu />,
